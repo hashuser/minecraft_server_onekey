1163ea0b2193caf4d44d73949037b4ab96f9eb1d
hashuser <60065064+hashuser@users.noreply.github.com>
2023-02-25 17:52:46 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ main(){
   apt install openjdk-19-jdk-headless
   java -Xmx${Memoey_max}M -Xms${Memoey_min}M -jar $(cd "$(dirname "$0")";pwd)/server.jar --nogui --initSettings
   echo "eula=true" > $(cd "$(dirname "$0")";pwd)/eula.txt
-  sed -i 's/gamemode=survival/gamemode=${Gmode}/g' $(cd "$(dirname "$0")";pwd)/server.properties
+  sed -i "s/gamemode=survival/gamemode=${Gmode}/g" "$(cd "$(dirname "$0")";pwd)/server.properties"
   cat $(cd "$(dirname "$0")";pwd)/server.properties
   create_service
   install_service
