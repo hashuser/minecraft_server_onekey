5ca1b9c4b7edd5db8f67040c9498cc572b87ad1c
hashuser <60065064+hashuser@users.noreply.github.com>
2023-02-25 16:38:35 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,11 +39,11 @@ main(){
   wget https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar
   apt search openjdk
   apt install openjdk-19-jdk-headless
-  java -Xmx${Memoey_max}M -Xms${Memoey_min}M -jar server.jar nogui
-  echo "eula=true" > eula.txt
-  sed -i 's/gamemode=servival/gamemode=${Gmode}/g' server.properties
   create_service
   install_service
+  echo "eula=true" > eula.txt
+  sed -i 's/gamemode=servival/gamemode=${Gmode}/g' server.properties
+  systemctl restart Minecraft.service
 }
 
 for arg in "$@"; do
